fbc0663c54e6eca5df1ddd63e81e526a70696036
saurav-tiwari03 <tiwarisaurav459@gmail.com>
2022-11-20 02:13:31 +0530

    Selection sorting using C++

---
+++ babar/selectionsorting.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+void print(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+void sorting(int arr[], int n)
+{
+    int min;
+    for (int i = 0; i < n; i++)
+    {
+        min = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+            swap(arr[min], arr[i]);
+        }
+    }
+}
+int main()
+{
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "unsorted array: ";
+    print(arr, n);
+    sorting(arr, n);
+    cout << "sorted array: ";
+    print(arr, n);
+}
